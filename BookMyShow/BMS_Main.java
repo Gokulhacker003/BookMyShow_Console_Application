f4668495fe9c439398d74db51698255989c8711b
Gokulhacker003 <129746126+Gokulhacker003@users.noreply.github.com>
2025-03-28 09:42:03 +0530

    Delete BookMyShow directory

--- BookMyShow/BMS_Main.java
+++
@@ -1,7 +0,0 @@
-public class BMS_Main {
-    // Main method to run the program
-    public static void main(String[] args) {
-        // Start the BookMyShow operations
-        BookMyShow_Ops.start();
-    }
-}
